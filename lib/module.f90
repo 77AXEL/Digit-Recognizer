289838a3446cd488f367f7eb482cfc3d47910383
Mohamed Gharbi <galaxysea777@gmail.com>
2025-12-08 16:58:34 +0100

    Add files via upload

---
+++ lib/module.f90
@@ -0,0 +1,21 @@
+module tensor
+contains
+  subroutine sum_vectors(V1, V2, T)
+    implicit none
+    real(8), intent(in)  :: V1(:)
+    real(8), intent(in)  :: V2(:)
+    real(8), intent(out) :: T(size(V1))
+    integer :: i
+    integer :: n
+
+    n = size(V1)
+    if (size(V2) /= n) then
+        print *, "Error: Vectors must have the same length"
+        stop
+    end if
+
+    do i = 1, n
+        T(i) = V1(i) + V2(i)
+    end do
+  end subroutine sum_vectors
+end module tensor
